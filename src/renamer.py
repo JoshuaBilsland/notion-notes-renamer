372f455cb0fe14059a19a33769b7856c6a70eb62
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-22 19:20:14 +0100

    feat: add Renamer constructor

---
+++ src/renamer.py
@@ -0,0 +1,9 @@
+class Renamer:
+    def __init__(self):
+        print("--------------------")
+        print("notion-notes-renamer")
+        print("--------------------")
+
+
+if __name__ == "__main__":
+    Renamer()

6ee14f2243ae413672491a903b7bb7450f145232
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-22 19:37:29 +0100

    feat: add display_menu()

--- src/renamer.py
+++ src/renamer.py
@@ -2,8 +2,16 @@ class Renamer:
     def __init__(self):
         print("--------------------")
         print("notion-notes-renamer")
-        print("--------------------")
+        print("--------------------\n")
 
+        print(self.__display_menu())
+
+    def __display_menu(self):
+        print("Menu Options")
+        print("------------")
+        print("f - rename a singular file")
+        print("d - rename all the files within a given directory")
+        print("\nUse 'help <command>' to get more information about a command")
 
 if __name__ == "__main__":
     Renamer()

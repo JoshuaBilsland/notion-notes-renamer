a753f84d2ec6de00ee64cf9058918f7385c47339
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-24 08:54:33 +0100

    feat: add user input handling

--- src/renamer.py
+++ src/renamer.py
@@ -13,5 +13,23 @@ class Renamer:
         print("d - rename all the files within a given directory")
         print("\nUse 'help <command>' to get more information about a command")
 
+    def __get_input(self):
+        return input("\n>> ")
+
+    def __handle_input(self, user_input):
+        split_input = user_input.split()
+        if len(split_input) == 2:
+            if split_input[0] == "f":
+                print()  # handle f
+            elif split_input[0] == "d":
+                print()  # handle d
+            elif split_input[0] == "help":
+                print()  # handle help
+            else:
+                print(f"ERROR: {split_input[0]} is not a menu option")
+        else:
+            print("ERROR: Invalid command. Try using 'help <command>'")
+
+
 if __name__ == "__main__":
     Renamer()

0c1e4ef7eff1515bc0abda404761d5240157bfe3
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-28 10:32:47 +0100

    feat: add checking if a file exists

--- src/renamer.py
+++ src/renamer.py
@@ -5,6 +5,8 @@ class Renamer:
         print("--------------------\n")
 
         print(self.__display_menu())
+        user_input = self.__get_input()
+        self.__handle_input(user_input)
 
     def __display_menu(self):
         print("Menu Options")
@@ -17,10 +19,10 @@ class Renamer:
         return input("\n>> ")
 
     def __handle_input(self, user_input):
-        split_input = user_input.split()
+        split_input = user_input.split(" ")
         if len(split_input) == 2:
             if split_input[0] == "f":
-                print()  # handle f
+                print()
             elif split_input[0] == "d":
                 print()  # handle d
             elif split_input[0] == "help":
@@ -29,6 +31,15 @@ class Renamer:
                 print(f"ERROR: {split_input[0]} is not a menu option")
         else:
             print("ERROR: Invalid command. Try using 'help <command>'")
+
+    def __file_exists(self, file_path):
+        try:
+            file = open(file_path, "r")
+        except FileNotFoundError:
+            return False
+        else:
+            file.close()
+            return True
 
 
 if __name__ == "__main__":

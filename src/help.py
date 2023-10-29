43f3a724e40d8a2b966b836a5673707422537ce9
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-29 19:48:39 +0000

    feat: add 'f' help

--- src/help.py
+++ src/help.py
@@ -6,3 +6,13 @@ def handle_help(self, split_input):
     else:
         return_value = f"ERROR: No help for {split_input[1]}"
     return return_value
+
+def get_f_command_help():
+    return """Rename a singular file by providing the path to it.
+
+To use this command, enter 'f' followed by the path to the file
+
+The path should be given as the plain path.
+
+Example: f F:\test_folder
+    """

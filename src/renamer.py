ac124df1145729c4871fcfbb6cc95d4572aecf63
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-29 09:34:58 +0000

    feat: add file renaming method

--- src/renamer.py
+++ src/renamer.py
@@ -35,9 +35,9 @@ class Renamer:
             if split_input[0] == "f":
                 print()
             elif split_input[0] == "d":
-                print()  # handle d
+                print()  # Handle d
             elif split_input[0] == "help":
-                print()  # handle help
+                print()  # Handle help
             else:
                 print(f"ERROR: {split_input[0]} is not a menu option")
         else:
@@ -52,6 +52,18 @@ class Renamer:
             file.close()
             return True
 
+    def __rename_file(self, file_name):
+        # EXAMPLE: 0fd35cb5-381e-4e8c-966c-897310c6095a_Mastering_Python_-_Everything_You_Need_To_Know_To_Become_a_Python_Master
+
+        first_underscore = file_name.find("_")
+
+        # Remove the start (0fd35...95a part)
+        removed_start = file_name[first_underscore:]
+
+        # Make all letters lowercase
+        lowered = removed_start.lower()
+
+        return lowered
 
 if __name__ == "__main__":
     Renamer()

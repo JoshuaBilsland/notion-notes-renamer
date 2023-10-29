834e5bf32c970a870b1d57e1532b2e2d9b44aa6e
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-10-29 20:09:47 +0000

    fix(help): correct various mistakes

--- src/help.py
+++ src/help.py
@@ -2,10 +2,11 @@ def handle_help(self, split_input):
     if split_input[1] == "f":
         return_value = get_f_command_help()
     elif split_input[1] == "d":
-        return_value get_d_command_help()
+        return_value = get_d_command_help()
     else:
         return_value = f"ERROR: No help for {split_input[1]}"
     return return_value
+
 
 def get_f_command_help():
     return """Rename a singular file by providing the path to it.
@@ -14,9 +15,10 @@ To use this command, enter 'f' followed by the path to the file.
 
 The path should be given as the plain path.
 
-Example: f F:\test_folder
+Example: f F:\test_folder\0fd35cb5-381e-4e8c-966c-897310c6095a_Mastering_Python_-_Everything_You_Need_To_Know_To_Become_a_Python_Master.pdf
     """
-    
+
+
 def get_d_command_help():
     return """Rename all the files within a directory.
 
@@ -24,7 +26,7 @@ To use this command, enter 'd' followed by the path to the directory.
 
 The path should be given as the plain path.
 
-Note: Ensure that the directory only contains files which need to be renamed.
+Note: Ensure that the directory only contains files that need to be renamed.
 
 Example: d F:\test_folder
 """
